5fdbcb3ae34e41c982c0c2a99910d38d24365ccb
sabilimaulana <spacysbl.learn@gmail.com>
2021-10-24 18:46:37 +0700

    Admin StoreGG  : Mengelola halaman kategori (CRUD)→ Create

--- app/category/controller.js
+++ app/category/controller.js
@@ -1,7 +1,28 @@
+const Category = require("./model");
+
 module.exports = {
   index: async (req, res) => {
     try {
       res.render("admin/category/view_category");
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  viewCreate: async (req, res) => {
+    try {
+      res.render("admin/category/create");
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  actionCreate: async (req, res) => {
+    try {
+      const { name } = req.body;
+
+      let category = Category({ name });
+      await category.save();
+
+      res.redirect("/category");
     } catch (error) {
       console.log(error);
     }
