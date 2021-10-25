de016f638ff15814f18a7a881b1d6d12762612c1
sabilimaulana <spacysbl.learn@gmail.com>
2021-10-25 19:55:12 +0700

    Admin StoreGG : Mengelola halaman nominal (CRUD)→Create

---
+++ app/nominal/controller.js
@@ -0,0 +1,105 @@
+const Nominal = require("./model");
+
+module.exports = {
+  index: async (req, res) => {
+    try {
+      const alertMessage = req.flash("alertMessage");
+      const alertStatus = req.flash("alertStatus");
+
+      const alert = { message: alertMessage, status: alertStatus };
+
+      const nominal = await Nominal.find();
+
+      res.render("admin/nominal/view_nominal", { nominal, alert });
+    } catch (error) {
+      req.flash("alertMessage", `${error?.message}`);
+      req.flash("alertStatus", "danger");
+
+      res.redirect("/category");
+      console.log(error);
+    }
+  },
+  viewCreate: async (req, res) => {
+    try {
+      res.render("admin/nominal/create");
+    } catch (error) {
+      console.log(error);
+      req.flash("alertMessage", `${error?.message}`);
+      req.flash("alertStatus", "danger");
+
+      res.redirect("/category");
+    }
+  },
+  actionCreate: async (req, res) => {
+    try {
+      const { coinName, coinQuantity, price } = req.body;
+
+      let nominal = Nominal({ coinName, coinQuantity, price });
+      await nominal.save();
+
+      req.flash("alertMessage", "Berhasil tambah nominal");
+      req.flash("alertStatus", "success");
+
+      res.redirect("/nominal");
+    } catch (error) {
+      console.log(error);
+      req.flash("alertMessage", `${error?.message}`);
+      req.flash("alertStatus", "danger");
+
+      res.redirect("/category");
+    }
+  },
+  // viewEdit: async (req, res) => {
+  //   try {
+  //     const { id } = req.params;
+
+  //     const category = await Category.findOne({ _id: id });
+  //     res.render("admin/category/edit", {
+  //       category,
+  //     });
+  //   } catch (error) {
+  //     console.log(error);
+  //     req.flash("alertMessage", `${error?.message}`);
+  //     req.flash("alertStatus", "danger");
+
+  //     res.redirect("/category");
+  //   }
+  // },
+  // actionEdit: async (req, res) => {
+  //   try {
+  //     const { id } = req.params;
+  //     const { name } = req.body;
+
+  //     await Category.findOneAndUpdate({ _id: id }, { name });
+
+  //     req.flash("alertMessage", "Berhasil ubah kategori");
+  //     req.flash("alertStatus", "success");
+
+  //     res.redirect("/category");
+  //   } catch (error) {
+  //     console.log(error);
+  //     req.flash("alertMessage", `${error?.message}`);
+  //     req.flash("alertStatus", "danger");
+
+  //     res.redirect("/category");
+  //   }
+  // },
+  // actionDelete: async (req, res) => {
+  //   try {
+  //     const { id } = req.params;
+
+  //     await Category.findOneAndRemove({ _id: id });
+
+  //     req.flash("alertMessage", "Berhasil hapus kategori");
+  //     req.flash("alertStatus", "success");
+
+  //     res.redirect("/category");
+  //   } catch (error) {
+  //     console.log(error);
+  //     req.flash("alertMessage", `${error?.message}`);
+  //     req.flash("alertStatus", "danger");
+
+  //     res.redirect("/category");
+  //   }
+  // },
+};
