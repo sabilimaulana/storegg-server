c41e2ed6e3fad3bfefffd96b7af67c921cb26edd
sabilimaulana <spacysbl.learn@gmail.com>
2021-10-29 17:40:34 +0700

    Admin StoreGG : Mengelola halaman bank (CRUD)→Update

--- app/bank/controller.js
+++ app/bank/controller.js
@@ -47,44 +47,41 @@ module.exports = {
       res.redirect("/bank");
     }
   },
-  // viewEdit: async (req, res) => {
-  //   try {
-  //     const { id } = req.params;
+  viewEdit: async (req, res) => {
+    try {
+      const { id } = req.params;
 
-  //     const nominal = await Nominal.findOne({ _id: id });
-  //     res.render("admin/nominal/edit", {
-  //       nominal,
-  //     });
-  //   } catch (error) {
-  //     console.log(error);
-  //     req.flash("alertMessage", `${error?.message}`);
-  //     req.flash("alertStatus", "danger");
+      const bank = await Bank.findOne({ _id: id });
+      res.render("admin/bank/edit", {
+        bank,
+      });
+    } catch (error) {
+      console.log(error);
+      req.flash("alertMessage", `${error?.message}`);
+      req.flash("alertStatus", "danger");
 
-  //     res.redirect("/nominal");
-  //   }
-  // },
-  // actionEdit: async (req, res) => {
-  //   try {
-  //     const { id } = req.params;
-  //     const { coinName, coinQuantity, price } = req.body;
+      res.redirect("/bank");
+    }
+  },
+  actionEdit: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { name, nameBank, noRekening } = req.body;
 
-  //     await Nominal.findOneAndUpdate(
-  //       { _id: id },
-  //       { coinName, coinQuantity, price }
-  //     );
+      await Bank.findOneAndUpdate({ _id: id }, { name, nameBank, noRekening });
 
-  //     req.flash("alertMessage", "Berhasil ubah nominal");
-  //     req.flash("alertStatus", "success");
+      req.flash("alertMessage", "Berhasil ubah bank");
+      req.flash("alertStatus", "success");
 
-  //     res.redirect("/nominal");
-  //   } catch (error) {
-  //     console.log(error);
-  //     req.flash("alertMessage", `${error?.message}`);
-  //     req.flash("alertStatus", "danger");
+      res.redirect("/bank");
+    } catch (error) {
+      console.log(error);
+      req.flash("alertMessage", `${error?.message}`);
+      req.flash("alertStatus", "danger");
 
-  //     res.redirect("/nominal");
-  //   }
-  // },
+      res.redirect("/bank");
+    }
+  },
   // actionDelete: async (req, res) => {
   //   try {
   //     const { id } = req.params;
--- app/bank/router.js
+++ app/bank/router.js
@@ -5,16 +5,16 @@ const {
   index,
   viewCreate,
   actionCreate,
-  // viewEdit,
-  // actionEdit,
+  viewEdit,
+  actionEdit,
   // actionDelete,
 } = require("./controller");
 
 router.get("/", index);
 router.get("/create", viewCreate);
 router.post("/create", actionCreate);
-// router.get("/edit/:id", viewEdit);
-// router.put("/edit/:id", actionEdit);
+router.get("/edit/:id", viewEdit);
+router.put("/edit/:id", actionEdit);
 // router.delete("/delete/:id", actionDelete);
 
 module.exports = router;
