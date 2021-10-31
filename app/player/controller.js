21edac202300de387673047de211a3ab13402c1d
sabilimaulana <spacysbl.learn@gmail.com>
2021-10-31 19:35:24 +0700

    Api Player StoreGG : Membuat api detail page

--- app/player/controller.js
+++ app/player/controller.js
@@ -10,7 +10,30 @@ module.exports = {
 
       res.status(200).json({ data: voucher });
     } catch (error) {
-      res.status(500).json({ message: err.message || "Internal Server Error" });
+      res
+        .status(500)
+        .json({ message: error.message || "Internal Server Error" });
+    }
+  },
+  detailPage: async (req, res) => {
+    try {
+      const { id } = req.params;
+
+      const voucher = await Voucher.findOne({ _id: id })
+        .populate("category")
+        .populate("nominals")
+        .populate("user", "_id name phoneNumber");
+
+      if (!voucher) {
+        res.status(404).json({ message: "Voucher game tidak ditemukan!" });
+        return;
+      }
+
+      res.status(200).json({ data: voucher });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: error.message || "Internal Server Error" });
     }
   },
 };
--- app/player/router.js
+++ app/player/router.js
@@ -1,8 +1,9 @@
 var express = require("express");
 var router = express.Router();
 
-const { landingPage } = require("./controller");
+const { landingPage, detailPage } = require("./controller");
 
-router.get("/", landingPage);
+router.get("/landingpage", landingPage);
+router.get("/:id/detail", detailPage);
 
 module.exports = router;
