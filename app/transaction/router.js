38fc9674ef184e4e987950b8f36e2c2abcec6308
sabilimaulana <spacysbl.learn@gmail.com>
2021-10-31 17:02:13 +0700

    Admin StoreGG : Konfirmasi Transaksi dan Tolak Transaksi

--- app/transaction/router.js
+++ app/transaction/router.js
@@ -2,9 +2,10 @@ var express = require("express");
 const { isLoginAdmin } = require("../middlewares/auth");
 var router = express.Router();
 
-const { index } = require("./controller");
+const { index, actionStatus } = require("./controller");
 
 router.use(isLoginAdmin);
 router.get("/", index);
+router.put("/status/:id", actionStatus);
 
 module.exports = router;
