57096857aaf09d8522b3a407d1e2b72e769e6420
sabilimaulana <spacysbl.learn@gmail.com>
2021-11-11 16:14:06 +0700

    Api Player StoreGG : Membuat api checkout

--- app/player/router.js
+++ app/player/router.js
@@ -1,10 +1,12 @@
 var express = require("express");
+const { isLoginPlayer } = require("../middlewares/auth");
 var router = express.Router();
 
-const { landingPage, detailPage, category } = require("./controller");
+const { landingPage, detailPage, category, checkout } = require("./controller");
 
 router.get("/landingpage", landingPage);
 router.get("/:id/detail", detailPage);
 router.get("/category", category);
+router.post("/checkout", isLoginPlayer, checkout);
 
 module.exports = router;
