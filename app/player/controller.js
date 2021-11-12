ba754bd6b78993a71f30369fa2a01d11be0ff3ba
sabilimaulana <spacysbl.learn@gmail.com>
2021-11-12 16:38:39 +0700

    Api Player StoreGG : Membuat api history transaksi

--- app/player/controller.js
+++ app/player/controller.js
@@ -124,4 +124,41 @@ module.exports = {
         .json({ message: error.message || "Internal Server Error" });
     }
   },
+  history: async (req, res) => {
+    try {
+      const { status = "" } = req.query;
+      let criteria = {};
+
+      if (status.length) {
+        criteria = {
+          ...criteria,
+          status: { $regex: `${status}`, $options: "i" },
+        };
+      }
+
+      if (req.player._id) {
+        criteria = { ...criteria, player: req.player._id };
+      }
+
+      const history = await Transaction.find(criteria);
+
+      let total = await Transaction.aggregate([
+        { $match: criteria },
+        {
+          $group: {
+            _id: null,
+            value: { $sum: "$value" },
+          },
+        },
+      ]);
+
+      res
+        .status(201)
+        .json({ data: history, total: total.length ? total[0].value : 0 });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: error.message || "Internal Server Error" });
+    }
+  },
 };
--- app/player/router.js
+++ app/player/router.js
@@ -2,11 +2,18 @@ var express = require("express");
 const { isLoginPlayer } = require("../middlewares/auth");
 var router = express.Router();
 
-const { landingPage, detailPage, category, checkout } = require("./controller");
+const {
+  landingPage,
+  detailPage,
+  category,
+  checkout,
+  history,
+} = require("./controller");
 
 router.get("/landingpage", landingPage);
 router.get("/:id/detail", detailPage);
 router.get("/category", category);
 router.post("/checkout", isLoginPlayer, checkout);
+router.get("/history", isLoginPlayer, history);
 
 module.exports = router;
